25866af601d854a397cc3c2b51ecc505491faec6
CyrGdblssd <bogomazov.08@inbox.ru>
2024-05-16 21:32:14 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,40 +4,31 @@ let postsBox = document.getElementById("postsBox")
 let text = document.getElementById("text");
 let comment = document.getElementById("comment")
 let comments = document.getElementById("comments")
-
-let c = 35;
-
-let imgurls = [
+let c = 10;
+let images = [
     "pic2.png",
     "pic3.jpg",
     "pic1.png",
 ]
-
-
-let textIT = [
+let texts = [
     "Добрый день, это тоже РФМШ",
     "Добрый день, и это РФМШ",
     "Добрый день, это РФМШ",
 ]
-
 for (let i = 1; i < 100; i++) {
-    newtext = textIT[i % 3]
+    newtext = texts[i % 3]
     text.innerHTML = newtext
-    console.log(textIT[i % 3])
-
+    console.log(texts[i % 3])
     newpost = post.cloneNode(true);
     imgpost = newpost.getElementsByClassName("img")[0]
-    text.innerHTML = textIT[i % 3]
-    imgpost.setAttribute("src", imgurls[i % 3])
+    text.innerHTML = texts[i % 3]
+    imgpost.setAttribute("src", images[i % 3])
     postsBox.appendChild(newpost);
-
 }
-
 function like() {
     c++
     likeNumber.innerHTML = c
 }
-
 function addcomment() {
     let input = comment.value
     let addme = document.createElement("p")
